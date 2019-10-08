2869fa1f86f8b6e0d1f2415e3f3a1b9ae72759c5
JeanneBarriere <barriere.jeanne@gmail.com>
2019-10-08 17:15:39 +0200

    testboard

--- table.js
+++ table.js
@@ -1,14 +1,10 @@
 let rows = 6;
 let cols = 7;
-class Puissance4 {
-  constructor() {
-    this.rows = rows;
-    this.cols = cols;
-    this.board = Array(this.rows);
-    for (let i = 0; i < this.rows; i++) {
-      this.board[i] = Array(this.cols).fill(0);
-    }
-  }
-}
 
-let p4 = new Puissance4();
+let board = new Array(rows).fill(0);
+for (var i = 0; i < rows; i++)
+{
+ board[i] = new Array(cols).fill(0);
+};
+
+ let table = document.createElement('table');
