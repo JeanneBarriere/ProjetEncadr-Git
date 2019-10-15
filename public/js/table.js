40a9f6fa07b73011499fe7f0e49d9b08f262403a
JeanneBarriere <barriere.jeanne@gmail.com>
2019-10-15 16:09:36 +0200

    all

--- public/js/table.js
+++ public/js/table.js
@@ -12,22 +12,23 @@ class Puissance4 {
     this.winner = null;
     this.element = document.querySelector(element_id);
     this.element.addEventListener('click', (event) => handle_click(event,p4));
-    this.render();
-  }
 
-  render() {
+  }}
+
+  function render(p4) {
     let table = document.createElement('table');
-    for (let i = this.rows - 1; i >= 0; i--) {
+    for (let i = p4.rows - 1; i >= 0; i--) {
       let tr = table.appendChild(document.createElement('tr'));
-      for (let j = 0; j < this.cols; j++) {
+      for (let j = 0; j < p4.cols; j++) {
         let td = tr.appendChild(document.createElement('td'));
-        let colour = this.board[i][j];
+        let colour = p4.board[i][j];
         if (colour)
           td.className = 'player' + colour;
         td.dataset.column = j;
       }
     }
-    this.element.innerHTML = '';
-    this.element.appendChild(table);
-  }}
+    p4.element.innerHTML = '';
+    p4.element.appendChild(table);
+  }
   let p4 = new Puissance4('#game');
+  render(p4);
--- public/js/win.js
+++ public/js/win.js
@@ -1,31 +1,31 @@
-function win(colonne,ligne,joueur,nbjetonspourgagner){
-    return (winEnLigne(colonne,ligne,joueur,nbjetonspourgagner)); // || winEnColonne(colonne,ligne,joueur,nbjetonspourgagner) || winEnDiagonale(colonne,ligne,joueur,nbjetonspourgagner)) 
+function win(row, column, player, p4) {
+  // Horizontal
+  let count = 0;
+  for (let j = 0; j < p4.cols; j++) {
+    count = (p4.board[row][j] == player) ? count+1 : 0;
+    if (count >= 4) return true;
+  }
+  // Vertical
+  count = 0;
+  for (let i = 0; i < p4.rows; i++) {
+    count = (p4.board[i][column] == player) ? count+1 : 0;
+    if (count >= 4) return true;
+  }
+  // Diagonal
+  count = 0;
+  let shift = row - column;
+  for (let i = Math.max(shift, 0); i < Math.min(p4.rows, p4.cols + shift); i++) {
+    count = (p4.board[i][i - shift] == player) ? count+1 : 0;
+    if (count >= 4) return true;
+  }
+  // Anti-diagonal
+  count = 0;
+  shift = row + column;
+  for (let i = Math.max(shift - p4.cols + 1, 0); i < Math.min(p4.rows, shift + 1); i++) {
+    console.log(i,shift-i,shift)
+    count = (p4.board[i][shift - i] == player) ? count+1 : 0;
+    if (count >= 4) return true;
+  }
+
+  return false;
 }
-
-function winEnLigne(colonne,ligne,joueur,nbjetonspourgagner){
-	var min = (colonne - (MAX_COL/2) < 0) ? 0 : colonne - (MAX_COL/2);
-	var max = (colonne + (MAX_COL/2) > MAX_COL) ? MAX_COL : col + (MAX_COL/2);
-	var acc = 0;
-
-	for (var i = min; i <= max; i++) {
-		if (tab[i][ligne] == joueur) {
-			acc++;
-			if (acc == nbjetonspourgagner) return true;
-		}
-		else {
-			acc = 0;
-		}
-	}
-	return false;
-
-}
-/*
-function winEnColonne(colonne,ligne,joueur,nbjetonspourgagner){
-
-
-}
-
-function winEnDiagonale(colonne,ligne,joueur,nbjetonspourgagner){
-
-
-} */
