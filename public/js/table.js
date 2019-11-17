2363d63815d6332d2cdbc9a228766a734c1d18c5
samielatri <sami.elatri@yahoo.com>
2019-11-17 18:37:25 +0100

    musique de fond

--- public/js/table.js
+++ public/js/table.js
@@ -11,8 +11,7 @@ class Puissance4 {
     this.moves = 0;
     this.winner = null;
     this.element = document.querySelector(element_id);
-    this.element.addEventListener('click', (event) => handle_click(event,p4));
-
+    this.element.addEventListener('click', (event) => handle_click(event,p4));	
   }}
 
   function render(p4) {
@@ -33,6 +32,7 @@ class Puissance4 {
     p4.element.innerHTML = '';
     p4.element.appendChild(table);
   }
+
   let j1color = 1;
   let j2color = 6;
   let p4 = new Puissance4('#game');
