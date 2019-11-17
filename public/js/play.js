e9b41ea10f1586775119f32c360c4e568f9a0a7b
samielatri <sami.elatri@yahoo.com>
2019-11-17 18:37:32 +0100

    musique de fond

--- public/js/play.js
+++ public/js/play.js
@@ -5,12 +5,10 @@ function handle_click(event,p4) {
   }
   let column = event.target.dataset.column;
   if (column !== undefined) {
-    //column = parseInt(column);
     let row = play(column,p4);
 
     if (row === null) {
       window.alert("La colonne est pleine!");
-      // ajouter ici la fonction null qui verifie si toute les coolonnes sont pleines pour partie nulle, modifie la variable winner pour lancer la nouvelle partie
     } else {
       if (win(row, column, p4.turn, p4)) {
         p4.winner = p4.turn;
@@ -22,6 +20,7 @@ function handle_click(event,p4) {
      var audio = new Audio('sound/jeton.mp3');
      audio.play();
      tabplein(p4);
+     let first = 0 ; 
       switch (p4.winner) {
         case 1:
         var resultElem = document.getElementById("win");
@@ -40,7 +39,14 @@ function handle_click(event,p4) {
           resultElem.textContent = "Partie nulle";
           var victoire = new Audio('sound/nulle.mp3');
           victoire.play();
-
+	  break;
+	default:
+	  first++ ;
+	  const musique = new Audio('sound/cours.mp3') ; 
+	  if (counter === 1 && first === 1) {
+  	  	musique.play() ;
+	  }
+	  break;
       }
     }
   }
@@ -55,6 +61,9 @@ function reset(p4) {
   }
   p4.move = 0;
   p4.winner = null;
+  // Pour regler probleme musique ajouter : musique.stop();
+  // ok mais probleme : affichage de boite dialogue
+  // necessite debuggage mais aussi variables locales et globales visibles et justes pour ne plus interferer 
 }
 
 function set(row, column, player, p4) {
