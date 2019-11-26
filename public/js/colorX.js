f3fe1b14f74702b646cc64df3c7dfdcf862f2601
JeanneBarriere <barriere.jeanne@gmail.com>
2019-11-26 14:04:40 +0100

    Css

--- public/js/colorX.js
+++ public/js/colorX.js
@@ -7,56 +7,65 @@ function purple1(){
 function yellow1(){
   j1color = 6;
   render(p4);
-  document.getElementById("j1").style.color = "#4298EC";
+  document.getElementById("j1").style.color = "#F2EB30";
 }
 
 function orange1(){
   j1color = 3;
   render(p4);
-  document.getElementById("j1").style.color = "#985A28";
+  document.getElementById("j1").style.color = "#F88C36";
 }
 
 function green1(){
   j1color = 4;
   render(p4);
+  document.getElementById("j1").style.color = "#8BDD32";
 }
 
 function blue1(){
   j1color = 5;
   render(p4);
+  document.getElementById("j1").style.color = "#4298EC";
 }
 
 function red1(){
   j1color = 1;
   render(p4);
+  document.getElementById("j1").style.color = "#C92626";
 }
 
 function purple2(){
   j2color = 2;
   render(p4);
+  document.getElementById("j2").style.color = "#BF49BB";
 }
 
 function yellow2(){
   j2color = 6;
   render(p4);
+  document.getElementById("j2").style.color = "#F2EB30";
 }
 
 function orange2(){
   j2color = 3;
   render(p4);
+  document.getElementById("j2").style.color = "#F88C36";
 }
 
 function green2(){
   j2color = 4;
   render(p4);
+  document.getElementById("j2").style.color = "#8BDD32";
 }
 
 function blue2(){
   j2color = 5;
   render(p4);
+  document.getElementById("j2").style.color = "#4298EC";
 }
 
 function red2(){
   j2color = 1;
   render(p4);
+  document.getElementById("j2").style.color = "#C92626";
 }
