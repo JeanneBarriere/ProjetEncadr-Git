aa859f731461334599361cf4625b77d71b8ff82e
NilsM75 <compte.mxs@hotmail.fr>
2019-11-26 16:12:18 +0100

    ajout du fichier qui modifie le nb de lignes de notre p4

---
+++ public/js/nombreDeLignes.js
@@ -0,0 +1,6 @@
+function nombreDeLignes () {
+  var lign = document.getElementById('nombreDeLignes').value;
+}
+var el = document.getElementById('p4');
+el.addEventListener("click", nombreDeLignes, false);
+render(p4);
