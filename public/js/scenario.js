34d6ad97538b8c791ffcca826483a077d55353e0
JeanneBarriere <barriere.jeanne@gmail.com>
2020-01-16 00:15:15 +0100

    correction

--- public/js/scenario.js
+++ public/js/scenario.js
@@ -188,7 +188,7 @@ async function IA(Puissance4){
       var resultElem = document.getElementById("win");
       resultElem.textContent = "Victoire du premier joueur";
       var victoire = new Audio('sound/victoire.mp3');
-      level = (level <= 3) ? level+1 : 3 ;
+      level = (level < 3) ? level+1 : 3 ;
       victoire.play();
       break;
       case 2:
